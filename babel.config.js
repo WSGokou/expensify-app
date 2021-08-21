fb2fd263c3d89e62f69439aec7b71a7b2f432630
WSGokou <gokou097@gmail.com>
2021-08-21 18:46:53 +0100

    babel file edit

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,7 @@
 const plugins = [];
 
 if (process.env.NODE_ENV === "development") {
-<<<<<<< HEAD
-  // plugins.push("react-refresh/babel");
-=======
-//   plugins.push("react-refresh/babel");
->>>>>>> 607d7ed0fbf6fc364d7817c985b7b1229eb0f915
+  //  plugins.push("react-refresh/babel");
 }
 
 module.exports = {
